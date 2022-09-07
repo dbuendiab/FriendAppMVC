2bf20eff1d63c046d42ddcf2d71d3b7d867d32f4
dbuendiab <dbuendiab@yahoo.es>
2022-09-08 00:12:35 +0200

    Avoid repeat form if already exists

--- js/view.js
+++ js/view.js
@@ -5,22 +5,30 @@ class View {
     }
 
     newFriendFormShow() {
+        // Si ya existe el formulario, no hacer nada
+        if (document.querySelector("div .new-friend")) {
+            return
+        }
+        // Construir el formulario
         const form = document.createElement("div")
         form.className = "new-friend"
         // Necesito input boxes para name, date, importance, periodicity, note
         form.innerHTML = `
         <label>Nombre: <input type="text" id="name"></label>
-        <label>Prox. cita: <input type="date" id="date"></label>
+        <label>Próx. cita: <input type="date" id="date"></label>
         <label>Importancia: <input type="range" min="0" max="100" id="importance"></label>
         <label>Periodicidad: <input type="number" min="1" id="periodicity" value="7"></label>
         <label>Notas: <textarea id="note"></textarea></label>
         <button id="accept">Aceptar</button>
         <button id="cancel">Cancelar</button>
         `
+        // Añadir el formulario al DOM
         this.root.appendChild(form)
 
+        // El botón Accept recoge la información del nuevo friend
         const btnAccept = document.getElementById("accept")
         btnAccept.addEventListener("click", () => {
+            // Construir el objeto de datos para Friend
             const data = {
                 name: document.getElementById("name").value,
                 date: document.getElementById("date").value,
@@ -28,10 +36,13 @@ class View {
                 periodicity: document.getElementById("periodicity").value,
                 note: document.getElementById("note").value
             }
+            // Eliminar el formulario
             this.root.removeChild(form)
+            // Disparar el evento Nuevo Friend con los datos recogidos
             this.newFriendAddedEvent.trigger(data)
         })
 
+        // El botón Cancel elimina el formulario, sin hacer nada más
         const btnCancel = document.getElementById("cancel")
         btnCancel.addEventListener("click", () => {
             this.root.removeChild(form)
